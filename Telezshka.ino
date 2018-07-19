6857055e855d4a3e4c7cf2a3e75cb5709d800634
Олег Моисеев <oleg-98@list.ru>
2018-07-19 12:48:29 +0300

    Turn of 2 wheels works

--- Telezshka.ino
+++ Telezshka.ino
@@ -1,184 +1,175 @@
 #include <Array.h>
 //#include <PID_v1.h>
 
-#define mosfet 10
-#define reverse 8
-
 const double koef = 94 / 75;
-int ptp1 = A1;
-int ptp2 = A2;
-int ptp3 = A3;
+
 const double eps = 1.0;
 Array<double, 6> xyz;
 
 
-double Setpoint, Input, Output;
- 
+//double Setpoint, Input, Output;
 //PID myPID(&Input, &Output, &Setpoint, 1, 4, 3, DIRECT);
 
+const int potent1 = A1;
+const int potent2 = A2;
+const int potent3 = A3;
 
-class Data
+const int motorSpd1 = 2;
+const int motorReverse1 = 22;
+
+const int motorSpd2 = 3;
+const int motorReverse2 = 23;
+
+class wheel
 {
-  private:
-    double deg_1;
-    double deg_2;
-    double deg_3;
-    double spd_1;
-    double spd_2;
-    double spd_3;
-    int getData();
+    /*const int potentPin;
+    const int motorSpdPin;
+    const int motorReverseTurnPin;
+    const double turnEps;
+    const double koefPID;*/
 
-  public:
-    Data();
-    int initSerial();
+    int potentPin;
+    int motorSpdPin;
+    int motorReverseTurnPin;
+    double turnEps;
+    double koefPID;
+
+
+    int stopTurn();
+    /*TODO: Realize it!
+    int stopMove();
+    */
+    int standAngle(double angle);
+    int setTurnSpd(double spd);
+    int rollClockwise();
+    int rollCounterClock();
+
+    double angleNow();
+
     int moveHome();
-    int movePoint();
+    
+  public:
+    wheel(int pot, int mS, int mR);
+    int initialize();
+    int moveToAngle(double angle);
+    int setSpd(double spd);
+    
 };
 
-Data::Data()
-  : deg_1(165), deg_2(165), deg_3(165),
-    spd_1(0), spd_2(0), spd_3(0)
+wheel::wheel(int pot, int mS, int mR)
+:
+  potentPin(pot),
+  motorSpdPin(mS),
+  motorReverseTurnPin(mR),
+  turnEps(1.0),
+  koefPID(255. / 330.)
 {
 }
 
-int Data::initSerial()
+int wheel::initialize()
 {
-  Serial.begin(9600);
-}
-
-int moveHome()
-{
-  moveToAngle(165.0, ptp1);
-}
-
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(mosfet, OUTPUT);
-  pinMode(reverse, OUTPUT);
-
+  pinMode(motorSpdPin, OUTPUT);
+  pinMode(motorReverseTurnPin, OUTPUT);
+  pinMode(potentPin, INPUT);
+  
   moveHome();
 }
 
-void stopMove()
+int wheel::moveHome()
 {
-  Serial.println("Остановка");
-  analogWrite(mosfet, 0);
+  moveToAngle(165.0);
+}
+int wheel::stopTurn()
+{
+  analogWrite(motorSpdPin, 0);
 }
 
-double AngleNow1()
+int wheel::moveToAngle(double angle)
 {
-  return analogRead(ptp1) * 330.0 / 1024.0;
+  angle *= koef;
+  if ((angle <= 330) && (angle >= 0))
+  {
+    standAngle(angle);
+  }  
 }
 
-double AngleNow2()
+int wheel::standAngle(double angle)
 {
-  return analogRead(ptp2) * 330.0 / 1024.0;
-}
-
-double AngleNow3()
-{
-  return 3300.0 * ((5.0 / ((analogRead(ptp3) * 5.0) / 1024.0) ) - 1)  * 330.0 / 6300.0;
-}
-
-void rollClockwise()
-{
-  digitalWrite(reverse, HIGH);
-}
-
-void rollCounterClock()
-{
-  digitalWrite(reverse, LOW);
-}
-
-
-
-void setRotSpeed(double spd)
-{
-  spd > 0 ? rollClockwise() : rollCounterClock();
-  Serial.print("Send: ");
-  Serial.print(spd);
-  Serial.print(" ");
-
-  analogWrite(mosfet, abs(spd));  
-}
-
-void standAngle(double degFinish, int numPTP)
-{
-  double epsilon = 1.0;
   double spd;
-  double k = 255. / 330.;
-  //double k = 1;
   double minDelta = 45;
-  double prevSpd = 0;
+  
   do
   {
     do
     {
-      double nowA = AngleNow(numPTP);
-      
-      double delta = nowA - degFinish;
-  
+      double delta = angleNow() - angle;
+
       if (abs(delta) < minDelta)
       {
-        delta > 0 ? spd = minDelta * k : spd = -minDelta * k;
-  
+        delta > 0 ? spd = minDelta * koefPID : spd = -minDelta * koefPID;
       }
       else
       {
-        spd = delta * k;
+        spd = delta * koefPID;
       }
 
-      setRotSpeed(spd);
-
-      Serial.print("Angle now: ");
-      Serial.print(nowA);
+      setTurnSpd(spd);
+      Serial.print("Angle: ");
+      Serial.print(angleNow());
       Serial.print(" Speed: ");
       Serial.println(spd);
     }
-    while (abs(degFinish - AngleNow(numPTP)) > epsilon);
-    delay(10);
+    while (abs(angle - angleNow()) > turnEps);
+    delay(50);
   }
-  while (abs(degFinish - AngleNow(numPTP)) > epsilon);
-  stopMove();
+  while (abs(angle - angleNow()) > turnEps);
+  stopTurn();
+  Serial.println("Stopped!");
 
-  Serial.println(abs(degFinish - AngleNow(numPTP)) > epsilon);
-  Serial.println("EXITED");
-  Serial.println(degFinish - AngleNow(numPTP));
   
 }
+
+double wheel::angleNow()
+{
+  return analogRead(potentPin) * 330.0 / 1024.0;
+}
+
+int wheel::setTurnSpd(double spd)
+{
+  spd > 0 ? rollClockwise() : rollCounterClock();
+  analogWrite(motorSpdPin, abs(spd));
+}
+
+int wheel::rollClockwise()
+{
+  digitalWrite(motorReverseTurnPin, HIGH);
+}
+
+int wheel::rollCounterClock()
+{
+  digitalWrite(motorReverseTurnPin, LOW);
+}
+
+
+wheel wheel1(potent1, motorSpd1, motorReverse1);
+wheel wheel2(potent2, motorSpd2, motorReverse2);
+
+void setup()
+{
+  Serial.begin(9600);
+  //Serial.println("1st started");
+  wheel1.initialize();
+  //Serial.println("1st stopped\n2nd started");
+  wheel2.initialize();
+  //Serial.println("2nd stopped");
+}
+
 //180.0 0.0 0.0 0.0 0.0 0.0
-
-
-double AngleNow(int numPTP)
-{
-  switch (numPTP)
-  {
-    case A1 : return AngleNow1();
-    case A2 : return AngleNow2();
-    case A3 : return AngleNow3();
-  }
-}
-
-
-void moveToAngle(double degFinish, int numPTP)
-{
-  degFinish *= koef;
-  if ((degFinish <= 330) && (degFinish >= 0))
-  {
-    standAngle(degFinish, numPTP);
-  }
-}
-
 
 void moveTelejka(Array<double, 6> &xyz)
 {
-  moveToAngle(xyz[0], ptp1);
-  //moveWheel(xyz[1]);
-  // moveToAngle(xyz[2], eps, PTP2);
-  //moveWheel(xyz[3], PTP2);
-  //moveToAngle(xyz[4], eps, PTP3);
-  // moveWheel(xyz[5], PTP3);`
+  wheel1.moveToAngle(xyz[0]);
+  wheel2.moveToAngle(xyz[0]);
 
 }
 
@@ -186,11 +177,11 @@ void loop()
 {
   if (Serial.available() > 0)
   {
-      for (int i = 0; i < 6; ++i)
-      {
-        xyz.at(i) = Serial.parseFloat();
-        Serial.println(xyz.at(i));
-      }
-      moveTelejka(xyz);
+    for (int i = 0; i < 6; ++i)
+    {
+      xyz.at(i) = Serial.parseFloat();
+      Serial.println(xyz.at(i));
+    }
+    moveTelejka(xyz);
   }
 }
